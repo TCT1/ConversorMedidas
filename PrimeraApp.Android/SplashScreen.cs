53a5288e679b33c97305bf15d5886a2d01758e24
Leonardo JV <leonjv08@gmail.com>
2023-01-20 18:52:05 -0600

    commit

---
+++ PrimeraApp.Android/SplashScreen.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimeraApp.Droid
+{
+    [Activity(Label = "Conversiones", Icon = "@drawable/iconoCinta", Theme = "@style/nuevotema", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize)]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
---
+++ PrimeraApp/Views/Convertir.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PrimeraApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Convertir : ContentPage
+    {
+        double cm;
+        double m;
+
+        public Convertir()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcular()
+        {
+            cm = Convert.ToDouble(txt.Text);
+            m = cm / 100;
+            lblResultado.Text = m.ToString() + " m";
+        }
+
+        private void Validar()
+        {
+            if (!string.IsNullOrEmpty(txt.Text))
+            {
+                Calcular();
+            }
+            else
+            {
+                DisplayAlert("Error", "Ingrese una cantidad", "OK");
+            }
+        }
+
+        private void btnvolver_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        private void btncalcular_Clicked(object sender, EventArgs e)
+        {
+            Validar();
+        }
+    }
+}
